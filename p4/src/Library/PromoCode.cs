34a890db77158844dd80e58cfddd47ad03cbf3c1
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-12-01 21:02:13 -0300

    ejer04.1 incompleto

--- p4/src/Library/PromoCode.cs
+++ p4/src/Library/PromoCode.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Ucu.Poo.Defense
 {
-    public class PromoCode
+    public class PromoCode: IOfferItem
     {
         private string code {get;set;}
         private int amount;
@@ -15,6 +15,10 @@ namespace Ucu.Poo.Defense
             }
             set
             {
+                if (value < 0)
+                {
+                    throw new ArgumentException("La cantidad debe ser mayor a 0");
+                }
                 this.amount = value;
             }
         }
